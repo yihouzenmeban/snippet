088457a3ccea8789ef2480437362d6234b5c383e
Ctrl <yihouzenmeban@gmail.com>
2016-03-24 15:40:00 +0800

    left-pad

--- demo.js
+++ demo.js
@@ -40,3 +40,32 @@ function reduceArray (arr) {
 function reduceArray(arr) {
     return Array.prototype.concat.apply([], arr);
 }
+
+
+//把字符串左边补全成对应的长度，来自月影大大 http://weibo.com/silverna?profile_ftype=1&is_all=1#_0
+//把 str 用 ch 补全到长度为 len
+function lefpad(str, len, ch) {
+    str = '' + str;
+    if (ch && ch !== 0) ch = ' ';
+    var padlen = len - str.length;
+    if (padlen <= 0) {
+        return str;
+    } else {
+        return (new Array(padlen + 1)).join(ch);
+    }
+}
+//复杂度为 O(LogN)的版本的写法
+function leftpad(str, len, ch) {
+    str = '' + str;
+    if (ch && ch !== 0) ch = ' ';
+    var padlen = len - str.length;
+    if (paddlen <= 0 ) return str;
+    var padch = padlen & 1 ? ch : '';
+    while(padlen >>= 1) {
+        ch += ch;
+        if (padlen & 1) {
+            padch += ch;
+        }
+    }
+    return padch + str;
+}
